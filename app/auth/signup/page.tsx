37a63d64368af0e75990c5d90a71bf57bf9d6174
Erwin <antepuesto.erwin@gmail.com>
2025-04-14 15:00:02 +0800

    working user regis

--- app/auth/signup/page.tsx
+++ app/auth/signup/page.tsx
@@ -1,17 +1,34 @@
+"use client";
+
 import { Link, Button, TextField, Stack } from "@mui/material"
 import NextLink from "next/link"
+import createUser from "./create-user";
+import { useActionState } from "react";
 
 const Signup = () => {
+  const [state, formAction] = useActionState(createUser, { error: "" });
+
   return (
-    <div>
-      
-    <Stack spacing={2} className="w-full max-w-xs">
-      <TextField label="Email" type="email" variant="outlined" />
-      <TextField label="Password" type="password" variant="outlined" />
-      <Button variant="contained">Signup</Button>
-      <Link component={NextLink} href="/auth/login" className="self-center">Login</Link>
-    </Stack>
-    </div>
+    <form action={formAction} className="w-full max-w-xs">
+      <Stack spacing={2}>
+        <TextField 
+          name="email" 
+          label="Email" 
+          type="email" 
+          variant="outlined" 
+          helperText={state.error} 
+          error={!!state.error}/>
+        <TextField 
+          name="password" 
+          label="Password" 
+          type="password" 
+          variant="outlined" 
+          helperText={state.error} 
+          error={!!state.error}/>
+        <Button type="submit" variant="contained">Signup</Button>
+        <Link component={NextLink} href="/auth/login" className="self-center"> Login</Link>
+      </Stack>
+    </form>
   )
 }
 
