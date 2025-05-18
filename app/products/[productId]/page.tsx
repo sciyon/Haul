e586cf241b4863ae2512a61a1e84dabc62f3c208
Erwin <antepuesto.erwin@gmail.com>
2025-05-18 23:58:55 +0800

    images

--- app/products/[productId]/page.tsx
+++ app/products/[productId]/page.tsx
@@ -1,10 +1,35 @@
+import { Grid, Stack, Typography } from "@mui/material"
+import getProduct from "./get-product"
+import Image from "next/image"
+import { getProductImage } from "../product-image"
+
 interface SingleProductProps{
   params: { productId: string }
 }
 
 export default async function SingleProduct({ params }: SingleProductProps){
+  const product = await getProduct(+params.productId)
   return (
-   <>
-   Single Product {params.productId}</> 
+    <Grid container marginBottom={"2rem"} rowGap={3}>
+      { product.imageExists && (
+        <Grid size={{ md:6, xs:12 }}>
+              <Image 
+                src={getProductImage(product.id)}
+                alt="Product Image"
+                width={0}
+                height={0}
+                className="w-full sm:w-3/4 h-auto"
+                sizes="100vw"
+              />
+        </Grid>
+      ) }
+      <Grid size={{ md:6, xs:12 }}>
+        <Stack gap={3} marginBottom={"2rem"}>
+          <Typography variant="h2">{product.name}</Typography>
+          <Typography>{product.description}</Typography>
+          <Typography variant="h4">${product.price}</Typography>
+        </Stack>
+      </Grid>
+    </Grid>
   )
 }
--- app/products/product.tsx
+++ app/products/product.tsx
@@ -1,32 +1,40 @@
-import { CardHeader, Stack } from "@mui/material"
+"use client"
+
+import { CardActionArea, CardHeader, Stack } from "@mui/material"
 import { Card } from "@mui/material"
 import { Typography } from "@mui/material"
 import { Product as IProduct } from "./interfaces/product.interface"
 import Image from "next/image"
 import { API_URL } from "../common/constants/api"
+import { getProductImage } from "./product-image"
+import { useRouter } from "next/navigation"
 
 interface ProductProps {
   product: IProduct
 }
 
 export function Product({ product }: ProductProps) {
+  const router = useRouter()
+
   return (
-    <Card className="p-4">
-      <Stack >
-        <Typography variant="h4">{product.name}</Typography>
-        { product.imageExists && (
-          <Image 
-            src={`${API_URL}/images/products/${product.id}.jpg`}
-            alt="Product Image"
-            width='0'
-            height='0'
-            className="w-full h-auto"
-            sizes="100vw"
-          />
-        )}
-        <Typography>{product.description}</Typography>
-        <Typography>${product.price}</Typography>
-      </Stack>
-    </Card>
+    <CardActionArea onClick={() => router.push(`/products/${product.id}`)}>
+      <Card className="p-4">
+        <Stack >
+          <Typography variant="h4">{product.name}</Typography>
+          { product.imageExists && (
+            <Image 
+              src={getProductImage(product.id)}
+              alt="Product Image"
+              width='0'
+              height='0'
+              className="w-full h-auto"
+              sizes="100vw"
+            />
+          )}
+          <Typography>{product.description}</Typography>
+          <Typography>${product.price}</Typography>
+        </Stack>
+      </Card>
+    </CardActionArea>
   )
 }
