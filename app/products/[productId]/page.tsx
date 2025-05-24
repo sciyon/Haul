eaa0b0ad10eacf7fdf1936d5bb6beda6c3907b0a
Erwin <antepuesto.erwin@gmail.com>
2025-05-25 01:28:47 +0800

    Stripe UI Integration

--- app/products/[productId]/page.tsx
+++ app/products/[productId]/page.tsx
@@ -2,6 +2,7 @@ import { Grid, Stack, Typography } from "@mui/material"
 import getProduct from "./get-product"
 import Image from "next/image"
 import { getProductImage } from "../product-image"
+import Checkout from "@/app/checkout/checkout"
 
 interface SingleProductProps{
   params: { productId: string }
@@ -28,6 +29,7 @@ export default async function SingleProduct({ params }: SingleProductProps){
           <Typography variant="h2">{product.name}</Typography>
           <Typography>{product.description}</Typography>
           <Typography variant="h4">${product.price}</Typography>
+          <Checkout productId={product.id} />
         </Stack>
       </Grid>
     </Grid>
