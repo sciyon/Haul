01e5b81462f5d935429ffe473c58c7c3ba8faed6
Erwin Antepuesto <antepuesto.erwin@gmail.com>
2025-05-30 22:50:58 +0800

    fixed issue when error renders when on landing but unauthenticated user

--- app/products/products.tsx
+++ app/products/products.tsx
@@ -2,11 +2,42 @@ import Grid from '@mui/material/Grid';
 import getProducts from "./actions/get-products"
 import { Product } from "./product"
 import ProductsGrid from './products-grid';
+import { Typography, Button } from '@mui/material';
+import Link from 'next/link';
+import authenticated from '../auth/actions/authenticated';
 
 export default async function Products() {
-  const products = await getProducts()
+  const isAuthenticated = await authenticated();
+  
+  if (!isAuthenticated) {
+    return (
+      <Grid container justifyContent="center" alignItems="center" sx={{ height: '85vh' }}>
+        <Grid item>
+          <Typography variant="h5" gutterBottom>
+            Please log in or sign up to view products
+          </Typography>
+          <div className="flex gap-2">
 
-  return (
-    <ProductsGrid products={products} />
-  )
+            <Button 
+              component={Link} 
+              href="/auth/login" 
+              variant="contained"
+            >
+              Login
+            </Button>
+            <Button 
+              component={Link} 
+              href="/auth/signup" 
+              variant="contained"
+            >
+              Sign Up
+            </Button>
+          </div>
+        </Grid>
+      </Grid>
+    );
+  }
+
+  const products = await getProducts();
+  return <ProductsGrid products={products} />;
 }
